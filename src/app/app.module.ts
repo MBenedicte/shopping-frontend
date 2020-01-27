c09605378a62522a45c6d767b585e3362a5cf96b
MBenedicte <mbenedicte20@gmail.com>
2020-01-27 15:39:59 +0200

    Create the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './home/header/header.component';
+import { BodyComponent } from './home/body/body.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    HomeComponent,
+    AuthComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
