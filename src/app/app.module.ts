39ddf58b85873f0c93a796ca0d3fc329643cd904
MBenedicte <mbenedicte20@gmail.com>
2020-01-27 16:44:13 +0200

    Add heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
-import { HeaderComponent } from './home/header/header.component';
-import { BodyComponent } from './home/body/body.component';
-import { FooterComponent } from './home/footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AuthComponent } from "./auth/auth.component";
+import { HeaderComponent } from "./home/header/header.component";
+import { BodyComponent } from "./home/body/body.component";
+import { FooterComponent } from "./home/footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent,
     HomeComponent,
     AuthComponent,
     HeaderComponent,
     BodyComponent,
     FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
