7f89a6b92b9119e74e5e8e80309bd02b06bb34ed
MBenedicte <mbenedicte20@gmail.com>
2020-01-27 19:01:59 +0200

    Start-heading

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {faPhone } from "@fortawesome/free-solid-svg-icons"
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  faPhone = faPhone;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
